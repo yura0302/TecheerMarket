fcdb48fbd41955604acec6a8b49d9bb64ce28d04
yura <yuraa0302@gmail.com>
2024-04-09 23:27:55 +0900

    feat : sentry 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,27 @@ import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
 import Modal from 'react-modal';
 import axios from 'axios';
+import * as Sentry from '@sentry/react';
 
 if (import.meta.env.DEV) {
   worker.start();
 }
+
+Sentry.init({
+  dsn: 'https://837a2b60adead0b63c0da05f4d9b81bc@o4507055682748416.ingest.us.sentry.io/4507055686942720',
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ['localhost', /^https:\/\/techeermarket\.site\//],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
