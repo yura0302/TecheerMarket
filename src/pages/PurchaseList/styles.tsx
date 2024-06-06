79b305005e17e097f896165fc33931fa7cbfef29
EunJu JO <dmswnehf@naver.com>
2024-06-06 23:40:17 +0900

    fix: 구매 내역 페이지 글쓰기 버튼 삭제

--- src/pages/PurchaseList/styles.tsx
+++ src/pages/PurchaseList/styles.tsx
@@ -1,26 +1,5 @@
 import styled from 'styled-components';
 
-export const BtnDiv = styled.div`
-  padding: 2.8rem 2.2rem 0;
-`;
-
-export const WriteBtn = styled.button`
-  width: 10.6rem;
-  height: 3.5rem;
-  appearance: none;
-  border: 0;
-  padding: 0;
-  background-color: transparent;
-  border-radius: 1rem;
-  background: #fd8944;
-  color: #fff;
-  font-size: 14px;
-  font-weight: 700;
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
 export const ProductContainer = styled.div`
   padding: 3rem 2rem 0;
 `;
